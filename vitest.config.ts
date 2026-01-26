c4760668228715ef8122739968109305d0acc239
miichom <hello@cammy.xyz>
2026-01-26 20:01:45 +0000

    chore: enforce coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    coverage: { provider: "v8", reporter: ["text", "json-summary", "lcov", "cobertura"] },
+    coverage: {
+      enabled: true,
+      provider: "v8",
+      reporter: ["text", "json-summary", "lcov", "cobertura"],
+      thresholds: { branches: 90, functions: 90, lines: 90, statements: 90 },
+    },
     environment: "node",
     globals: true,
     hookTimeout: 10_000,
