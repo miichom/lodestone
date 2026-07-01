0bccaf2723d698476f5f5b532067454a8e6c418b
miichom <hello@cammy.xyz>
2026-07-01 19:50:47 +0100

    chore: remove coverage thresholds and simplify timeout values in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,14 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      enabled: true,
       provider: "v8",
       reporter: ["text", "json-summary", "lcov", "cobertura"],
-      thresholds: { branches: 75, functions: 80, lines: 80, statements: 80 },
     },
     environment: "node",
     globals: true,
-    hookTimeout: 10_000,
-    testTimeout: 10_000,
+    hookTimeout: 10000,
+    testTimeout: 10000,
   },
 });
